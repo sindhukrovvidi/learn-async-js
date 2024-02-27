447ca068dae07660f99a7bc9b206b459f88b4b10
Sindhu Krovvidi <sindhukrovvidi@gmail.com>
2024-02-26 23:01:15 -0500

    in class activity

--- src/quiz/basic-server.js
+++ src/quiz/basic-server.js
@@ -37,17 +37,18 @@ const server = http.createServer((req, res) => {
     if (pathname.startsWith('/incidents/')) {
         // Extract parameters from the URL
         const [, , road, location, direction, id] = pathname.split('/');
+
         // Check if all parameters are provided
         if (road && location && direction && id) {
             // Send response with the extracted parameters
             res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(incidents.filter((incident) => {
-                return incident.id === `MABOS00${id}`
-            })));
+                res.end(JSON.stringify(incidents.filter((incident) => {
+                    return incident.id === `MABOS00${id}`
+                })));
         }
         else {
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('Endpoint not found');
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('Invalid parameters');
         }
     } else {
         // If the request is for an unsupported endpoint, send a not found response
