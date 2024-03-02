3309e34caac9bb167ab4da06351e9a91a1e5b30f
Sindhu Krovvidi <sindhukrovvidi@gmail.com>
2024-03-02 18:29:17 -0500

    Feb 27 activity

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -1,5 +1,41 @@
 const array2D = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, -9]
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, -9],
 ];
+
+function checkForNegatives(arr) {
+  return new Promise((resolve, reject) => {
+    if (Array.isArray(arr)) {
+      setTimeout(() => {
+        const rowPromises = arr.map((row, index) => {
+          return new Promise((resolve, reject) => {
+            if (row.some((num) => num < 0)) {
+              resolve(index);
+            } else {
+              reject("No row contains negative numbers");
+            }
+          });
+        });
+
+        Promise.any(rowPromises)
+          .then((rowIndex) => {
+            resolve(rowIndex);
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      }, 0);
+    } else {
+      reject("BAD INPUT: Expected array as input");
+    }
+  });
+}
+
+checkForNegatives(array2D)
+  .then((rowIndex) => {
+    console.log("Row index with negative number:", rowIndex);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -1,5 +1,29 @@
 const array2D = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
 ];
+
+function sum1DArray(arr) {
+  return new Promise((resolve, reject) => {
+    if (Array.isArray(arr)) {
+      setTimeout(() => {
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++) {
+          sum += arr[i];
+        }
+        resolve(sum);
+      }, 0);
+    } else {
+      reject("BAD INPUT: Expected array as input");
+    }
+    console.log("returning from sum");
+  });
+}
+
+Promise.all(array2D.map((i) => sum1DArray(i))).then((sumArray) => {
+  let sum = 0;
+  for (let i = 0; i < sumArray.length; i++) sum += sumArray[i];
+
+  console.log("sum: ", sum);
+});
